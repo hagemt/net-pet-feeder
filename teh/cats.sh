c80b796d1c41eb0e7380d90dcb2387f020f23f84
Tor E Hagemann <hagemt@users.noreply.github.com>
2021-08-23 18:53:25 +0000

    feat: add ~/.ssh/config example, naming

--- teh/cats.sh
+++ teh/cats.sh
@@ -5,8 +5,8 @@ set -o pipefail
 set -u
 #set -x
 
-: "${SELF_SERVER:=22222:localhost:22}"
-: "${SITE_SERVER:=four}"
+: "${SELF_SERVER:=22222:localhost:22}" # VPS jump box reaches back into RPi
+: "${SITE_SERVER:=vps}" # or "site" or "pi" you configured in ~/.ssh/config
 
 function _log {
 	echo 1>&2 "--- $(date --iso-8601=seconds --utc | cut -f1 -d+).000000Z $*"
